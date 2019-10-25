a7e9baf8fe69a539ba7165a0373861c8a4bc3560
Mustafa ÖZKAYA <mustafa.ozkayalar@gmail.com>
2019-10-25 09:42:00 +0300

    Add files via upload

---
+++ hCommKeyboard/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace hCommKeyboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.nfi = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnYenile = new System.Windows.Forms.Button();
+            this.cmbStop = new System.Windows.Forms.ComboBox();
+            this.cmbParite = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmdDBit = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBaud = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chkEnter = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Location = new System.Drawing.Point(276, 358);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(140, 72);
+            this.btnBaglan.TabIndex = 1;
+            this.btnBaglan.Text = "&Bağlan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // nfi
+            // 
+            this.nfi.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.nfi.BalloonTipTitle = "hOLOlu Comm Klavye Çevirici";
+            this.nfi.Text = "hOLOlu Comm Klavye Çevirici";
+            this.nfi.Visible = true;
+            this.nfi.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.nfi_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkEnter);
+            this.groupBox1.Controls.Add(this.btnYenile);
+            this.groupBox1.Controls.Add(this.cmbStop);
+            this.groupBox1.Controls.Add(this.cmbParite);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmdDBit);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbBaud);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 255);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bağlantı Bilgileri";
+            // 
+            // btnYenile
+            // 
+            this.btnYenile.Image = global::hCommKeyboard.Properties.Resources.iconfinder_arrow_circle_double_11450;
+            this.btnYenile.Location = new System.Drawing.Point(298, 42);
+            this.btnYenile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnYenile.Name = "btnYenile";
+            this.btnYenile.Size = new System.Drawing.Size(52, 35);
+            this.btnYenile.TabIndex = 14;
+            this.btnYenile.UseVisualStyleBackColor = true;
+            this.btnYenile.Click += new System.EventHandler(this.btnYenile_Click);
+            // 
+            // cmbStop
+            // 
+            this.cmbStop.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStop.FormattingEnabled = true;
+            this.cmbStop.Location = new System.Drawing.Point(112, 180);
+            this.cmbStop.Name = "cmbStop";
+            this.cmbStop.Size = new System.Drawing.Size(121, 28);
+            this.cmbStop.TabIndex = 13;
+            // 
+            // cmbParite
+            // 
+            this.cmbParite.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbParite.FormattingEnabled = true;
+            this.cmbParite.Location = new System.Drawing.Point(112, 146);
+            this.cmbParite.Name = "cmbParite";
+            this.cmbParite.Size = new System.Drawing.Size(121, 28);
+            this.cmbParite.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(48, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Parite :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(15, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Durma Biti :";
+            // 
+            // cmdDBit
+            // 
+            this.cmdDBit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmdDBit.FormattingEnabled = true;
+            this.cmdDBit.Location = new System.Drawing.Point(112, 112);
+            this.cmdDBit.Name = "cmdDBit";
+            this.cmdDBit.Size = new System.Drawing.Size(121, 28);
+            this.cmdDBit.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(28, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Data Biti :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(66, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hız :";
+            // 
+            // cmbBaud
+            // 
+            this.cmbBaud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBaud.FormattingEnabled = true;
+            this.cmbBaud.Location = new System.Drawing.Point(112, 78);
+            this.cmbBaud.Name = "cmbBaud";
+            this.cmbBaud.Size = new System.Drawing.Size(121, 28);
+            this.cmbBaud.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(60, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(112, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(82, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(362, 91);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "hOLOlu Comm Port Klavye Çevirici";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImage = global::hCommKeyboard.Properties.Resources.icons8_connect_100;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(-16, -6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 97);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chkEnter
+            // 
+            this.chkEnter.AutoSize = true;
+            this.chkEnter.Location = new System.Drawing.Point(112, 214);
+            this.chkEnter.Name = "chkEnter";
+            this.chkEnter.Size = new System.Drawing.Size(166, 24);
+            this.chkEnter.TabIndex = 15;
+            this.chkEnter.Text = "Sona ENTER Ekle";
+            this.chkEnter.UseVisualStyleBackColor = true;
+            this.chkEnter.CheckedChanged += new System.EventHandler(this.chkEnter_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(428, 442);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBaglan);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "hOLOlu Comm Klavye Çevirici";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.NotifyIcon nfi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBaud;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmdDBit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbStop;
+        private System.Windows.Forms.ComboBox cmbParite;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnYenile;
+        private System.Windows.Forms.CheckBox chkEnter;
+    }
+}
+
---
+++ hCommKeyboard/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace hCommKeyboard
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = new SerialPort(); // Seri port nesnesi oluşturuyoruz
+        const string userRoot = "HKEY_CURRENT_USER";
+        const string subkeyR = "Software\\hOLOlu";
+        const string subkey = "Software\\hOLOlu\\hCommKeyboard";
+        const string keyName = userRoot + "\\" + subkey;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nfi.Icon = this.Icon;
+
+            cmbBaud.Items.Clear();
+            cmbBaud.Items.Add("110");
+            cmbBaud.Items.Add("300");
+            cmbBaud.Items.Add("600");
+            cmbBaud.Items.Add("1200");
+            cmbBaud.Items.Add("2400");
+            cmbBaud.Items.Add("4800");
+            cmbBaud.Items.Add("9600");
+            cmbBaud.Items.Add("14400");
+            cmbBaud.Items.Add("19200");
+            cmbBaud.Items.Add("28800");
+            cmbBaud.Items.Add("38400");
+            cmbBaud.Items.Add("56000");
+            cmbBaud.Items.Add("57600");
+            cmbBaud.Items.Add("115200");
+            cmbBaud.Items.Add("128000");
+            cmbBaud.Items.Add("153600");
+            cmbBaud.Items.Add("230400");
+            cmbBaud.Items.Add("256000");
+            cmbBaud.Items.Add("460800");
+            cmbBaud.Items.Add("921600");
+            //cmbBaud.Text = "9600";
+
+            cmdDBit.Items.Clear();
+            cmdDBit.Items.Add("4");
+            cmdDBit.Items.Add("5");
+            cmdDBit.Items.Add("6");
+            cmdDBit.Items.Add("7");
+            cmdDBit.Items.Add("8");
+            //cmdDBit.Text = "8";
+            
+            cmbParite.Items.Clear();
+            cmbParite.Items.Add("None");
+            cmbParite.Items.Add("Odd");
+            cmbParite.Items.Add("Even");
+            cmbParite.Items.Add("Mark");
+            cmbParite.Items.Add("Space");
+            //cmbParite.Text = "None";
+
+            cmbStop.Items.Clear();
+            cmbStop.Items.Add("1");
+            cmbStop.Items.Add("1.5");
+            cmbStop.Items.Add("2");
+            //cmbStop.Text = "1";
+
+            AyarOku();
+
+            sp.BaudRate = Convert.ToInt32(cmbBaud.Text);  // Seri haberleşme hızını seçiyoruz (int32)
+            sp.DataBits = Convert.ToInt32(cmdDBit.Text); // göndereceğimiz bilginin kaç bitten oluşacağını bildiriyoruz (int32).
+
+            switch (cmbParite.SelectedIndex)
+            {
+                case 0:
+                    sp.Parity = Parity.None; // Eşlik bitidir. Gönderilen verinin doğruluğunu kontrol etmek için kullanılır. 
+                    break;
+
+                case 1:
+                    sp.Parity = Parity.Odd;
+                    break;
+
+                case 2:
+                    sp.Parity = Parity.Even;
+                    break;
+
+                case 3:
+                    sp.Parity = Parity.Mark;
+                    break;
+
+                case 4:
+                    sp.Parity = Parity.Space;
+                    break;
+            }
+
+            switch (cmbStop.SelectedIndex)
+            {
+                case 0:
+                    sp.StopBits = StopBits.One; // Stop bitinin kaç bit olacağını belirtir.
+                    break;
+
+                case 1:
+                    sp.StopBits = StopBits.OnePointFive;
+                    break;
+
+                case 2:
+                    sp.StopBits = StopBits.Two;
+                    break;
+            }
+
+            //SerialPort sp = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+            Yenile();
+
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+
+            sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sp.Close();
+            AyarKaydet();
+        }
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "")
+            {
+                AyarKaydet();
+
+                if (sp.IsOpen)
+                    sp.Close();
+
+                sp.PortName = comboBox1.Text;
+                try
+                {
+                    sp.Open(); // Seri portumuzu açıyoruz
+                }
+                catch (Exception ex)
+                {
+                }
+            } else
+            {
+                MessageBox.Show("Lütfen Com Portu Seçiniz..!");
+            }
+
+            if (sp.IsOpen) {
+                btnBaglan.Enabled = false;
+                groupBox1.Enabled = false;
+            } else {
+                btnBaglan.Enabled = true;
+                groupBox1.Enabled = true;
+            }
+        }
+
+        private void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] veri = new byte[sp.ReadBufferSize];
+            sp.Read(veri, 0, sp.ReadBufferSize);
+
+            //int veri = sp.ReadChar(); // Seri porttan integer türünden 1 karakter veri okur
+            SendKeys.SendWait(cevir(veri));
+
+            if (chkEnter.Checked) SendKeys.SendWait("{ENTER}");
+        }
+
+        private string cevir(byte[] gelen)
+        {
+            return System.Text.Encoding.UTF8.GetString(gelen).TrimEnd('\0');
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                nfi.Visible = true;
+                nfi.BalloonTipText = "Sistem Çalışıyor..!";
+                nfi.ShowBalloonTip(1000);
+            }
+        }
+
+        private void nfi_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            nfi.Visible = false;
+        }
+
+        private void btnYenile_Click(object sender, EventArgs e)
+        {
+            Yenile();
+        }
+
+        private void Yenile()
+        {
+                    comboBox1.Items.Clear();
+
+            string[] portlar = SerialPort.GetPortNames(); // Bağlı seri portları diziye aktardık
+            foreach (string portAdi in portlar)
+            {
+                comboBox1.Items.Add(portAdi);
+            }
+        }
+
+        private void chkEnter_CheckedChanged(object sender, EventArgs e)
+        {
+            AyarKaydet();
+        }
+
+        private void AyarOku()
+        {
+            //RegistryKey register;
+            //register = Registry.LocalMachine.OpenSubKey(@subkey, true);
+            //if (register.GetValue("ENTER") == null)
+            //{ }
+            //Registry.CurrentUser.CreateSubKey(@subkeyR);
+            Registry.CurrentUser.CreateSubKey(@subkey);
+
+            chkEnter.Checked = (bool)Registry.GetValue(keyName, "ENTER", false);
+            cmdDBit.Text = (string)Registry.GetValue(keyName, "DBit", "8"); 
+            cmbBaud.Text = (string)Registry.GetValue(keyName, "Baud", "9600"); 
+            cmbParite.Text = (string)Registry.GetValue(keyName, "Parite", "None"); 
+            cmbStop.Text = (string)Registry.GetValue(keyName, "Stop", "1"); 
+        }
+
+        private void AyarKaydet()
+        {
+            //Registry.CurrentUser.CreateSubKey(@subkeyR);
+            Registry.CurrentUser.CreateSubKey(@subkey);
+            if (comboBox1.Text != "") { 
+            Registry.SetValue(keyName, "ENTER", chkEnter.Checked);
+            Registry.SetValue(keyName, "DBit", cmdDBit.Text);
+            Registry.SetValue(keyName, "Baud", cmbBaud.Text);
+            Registry.SetValue(keyName, "Parite", cmbParite.Text);
+            Registry.SetValue(keyName, "Stop", cmbStop.Text);
+            }
+        }
+    }
+}
